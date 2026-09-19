4bd20bde0dc721ec43cb931ca3fe59e9cdbcaa76
raj <rajshetye.5855@gmail.com>
2026-09-19 10:51:43 +0530

    j

--- dashboard/src/app/App.jsx
+++ dashboard/src/app/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from '../routes/index'
 import ScrollToTop from '../components/common/generic/ScrollToTop'
+import Cursor from '../components/common/animation/Cursor'
 import { ToastContainer } from '../components/common/Toast'
 
 const App = () => {
     return (
         <BrowserRouter>
             <ScrollToTop />
+            <Cursor />
             <AppRoutes />
             <ToastContainer />
         </BrowserRouter>
